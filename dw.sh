64b01cdb924d9aa6b5eb34b1f7da7f913bfa6339
bagool185 <ioan.adrian98@gmail.com>
2017-07-24 23:23:19 +0300

    Create dw.sh

---
+++ dw.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#check whether there is only one parameter passed
+if [ "$#" -ne 1]
+then
+  echo "Illegal number of parameters"
+else 
+  #otherwise, download the mp3
+  youtube-dl --extract-audio --audio-format mp3 --prefer-ffmpeg $1
+fi
