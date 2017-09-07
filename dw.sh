51a6cd5fb802a9ba32f94af84056cb3846c9f902
Adrian Ioan <ioan.adrian98@gmail.com>
2017-09-07 12:10:29 +0300

    Update dw.sh

--- dw.sh
+++ dw.sh
@@ -7,4 +7,7 @@ then
 else 
   #otherwise, download the mp3
   youtube-dl --extract-audio --audio-format mp3 --prefer-ffmpeg --output "%(title)s.%(ext).s" $1
+  # uncomment the next line if you want underscores to be replaced
+  # with whitespaces
+  #for f in *.mp3; do mv "$f" "${f//_/ }"; done
 fi
